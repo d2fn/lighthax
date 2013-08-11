d702e490c6171998e27668a966c328aefbf77836
Dietrich Featherston <d@github.com>
2013-08-11 13:47:54 -0700

    /locations and /locations/:id endpoints for easily denormalizing all fixtures associated with a region - sub resources (in progress) can blink locations or set the lighting level

--- bin/lighthax.rb
+++ bin/lighthax.rb
@@ -2,111 +2,11 @@
 
 $:.unshift File.expand_path "../../lib", __FILE__
 
-require 'net/http'
 require 'sinatra'
-require 'httparty'
 require 'json'
-require 'lighthax/payload'
 
-class RwHttp
-  def initialize(address, u, p)
-    @address = address
-    @auth = { :username => u, :password => p }
-  end
-  def get(path)
-    puts "#{@address}/#{path}"
-    response = HTTParty.get("#{@address}/#{path}", { :basic_auth => @auth, :verify => false, :ssl_version => :TLSv1 })
-    JSON.parse(response)
-  end
-  def post(path, data)
-    HTTParty.post("#{@address}/#{path}", { :headers => {'Content-type' => 'application/x-www-form-urlencoded'}, :body => data, :format => :plain, :basic_auth => @auth, :verify => false, :ssl_version => :TLSv1 })
-  end
-end
-
-class Lights
-  attr_accessor :serial_numbers
-  attr_accessor :name_to_serial_number
-  attr_accessor :serial_number_to_name
-
-  def initialize(rwhttp)
-    @rwhttp = rwhttp
-  end
-
-  def lights
-    if @lights
-      @lights
-    else
-      fixtures
-      @lights
-    end
-  end
-
-  def fixtures
-    if @fixtures
-      @fixtures
-    else
-      @fixtures = @rwhttp.get("rApi/fixture")
-      parse_fixtures
-      @fixtures
-    end
-  end
-
-  def light(serial)
-    @lights[serial]
-  end
-
-  def locations
-    if @locations
-      @locations
-    else
-      @locations = @rwhttp.get("rApi/location")
-      parse_locations
-      @locations
-    end
-  end
-
-  def parse_fixtures
-    @serial_numbers = []
-    @serial_number_to_name = {}
-    @name_to_serial_number = {}
-    @lights = {}
-    @fixtures.each do |f|
-      sn = f["serialNum"]
-      name = f["name"]
-      f["href"] = "/lights/#{sn}"
-      @serial_numbers << sn
-      @serial_number_to_name[sn] = name
-      @name_to_serial_number[name] = sn
-      @lights[sn] = f
-    end
-  end
-
-  def parse_locations
-    @locations = {}
-  end
-
-  def set_light_brightness(serial, level)
-  end
-
-  def set_location_brightness(id, level)
-  end
-
-  def blink_location_on(id)
-  end
-
-  def blink_location_off(id)
-  end
-
-  def blink_on(serials)
-    payload = Lighthax::PayloadGenerator.blink(serials, true).to_json + "====="
-    @rwhttp.post("cgi-bin/model.cgi", payload)
-  end
-
-  def blink_off(serials)
-    payload = Lighthax::PayloadGenerator.blink(serials, false).to_json + "====="
-    @rwhttp.post("cgi-bin/model.cgi", payload)
-  end
-end
+require 'lighthax/rwhttp'
+require 'lighthax/lights'
 
 address = ENV['RW_ADDRESS']
 user = ENV['RW_USER']
@@ -114,7 +14,6 @@ pass = ENV['RW_PASS']
 
 rwhttp = RwHttp.new(address, user, pass)
 lights = Lights.new(rwhttp)
-lights.fixtures
 
 set :bind, "0.0.0.0"
 
@@ -134,22 +33,22 @@ get '/lights/:serial' do
 end
 
 post '/lights/:serial/brightness/:level' do
-  content-type :json
+  content_type :json
   lights.set_light_brightness(params[:serial], params[:level]).to_json
 end
 
 get '/locations' do
   content_type :json
-  lights.locations.to_json
+  lights.list_locations.to_json
 end
 
 get '/locations/:id' do
   content_type :json
-  lights.locations.to_json
+  lights.location_by_id(params[:id].to_i).to_json
 end
 
 post '/locations/:id/brightness/:level' do
-  content-type :json
+  content_type :json
   lights.set_location_brightness(params[:id], params[:level]).to_json
 end
 
